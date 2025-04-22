269aa5d5eaae927a079a8c9f9c79ecd709b2d4ac
laila <laila.teofilo@ufu.br>
2025-04-22 15:35:53 -0300

    Atividade 2.1

--- Questão 2.ipynb/exemplo01.py
+++ Questão 2.ipynb/exemplo01.py
@@ -1,1 +1,22 @@
-.py
+#Lançamento Oblíquo
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+
+#Posições iniciais, velocidade inicial e massa
+(x,y) = (0,0) m
+(vx,vy) = (10,10) m/s
+massa = 1.0 kg
+#Força da gravidade
+(fx,fy)= (0,-9.81)  m/s²
+#Intervalo de tempo
+dt = 0.1 s
+
+def__init__(self, x, y, vx, vy, massa): # construtor da classe que inicializa os atributos da partícula.
+    self.x = x
+    self.y = y
+    self.vx = vx
+    self.vy = vy
+    self.massa = massa
+    self.tempo = 0.0 # tempo inicial
