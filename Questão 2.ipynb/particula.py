ed64e24d1e084a51d83d2e7e734056c2e057373a
laila <laila.teofilo@ufu.br>
2025-04-22 15:02:40 -0300

    Descrição do código

--- Questão 2.ipynb/particula.py
+++ Questão 2.ipynb/particula.py
@@ -5,3 +5,7 @@ y: coordenada y da partícula
 vx: velocidade na direção x
 vy: velocidade na direção y
 massa: massa da partícula
+
+__init__(self, x, y, vx, vy, massa): construtor da classe que inicializa os atributos da partícula.
+newton(self, fx, fy, dt): aplica a segunda lei de Newton para atualizar a velocidade e a posição da partícula com base nas forças (fx, fy) aplicadas e no intervalo de tempo dt.
+Considerar que a força é constante durante o intervalo de tempo dt.
